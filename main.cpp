8a319f8e4b1ef0951c08feddce3a5bc9b379a45e
Hana-kuragechan <k023g1049@g.neec.ac.jp>
2024-07-26 14:01:58 +0900

    08_01確認課題完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include<stdio.h>
 
+#include<string>
+#include<iostream>
+#include<chrono>
+
 int main() {
 
+	std::string a(100000, 'a');
+
+	std::chrono::system_clock::time_point start1, end1;
+	std::chrono::system_clock::time_point start2, end2;
+
+	start1 = std::chrono::system_clock::now();
+	std::string b = a;
+	end1= std::chrono::system_clock::now();
+
+
+	start2 = std::chrono::system_clock::now();
+	std::string c = a;
+	end2 = std::chrono::system_clock::now();
+
+	auto time1 = end1 - start1;
+	auto time2 = end2 - start2;
+	auto m1 = std::chrono::duration_cast<std::chrono::microseconds>(time1).count();
+	auto m2 = std::chrono::duration_cast<std::chrono::microseconds>(time2).count();
+
+	std::cout << "10000文字をコピーと移動で比較" << std::endl;
+	std::cout<<"コピー"<<m1<<"μs" << std::endl;
+	std::cout<<"コピー"<<m2<<"μs" << std::endl;
 
 	return 0;
 }
